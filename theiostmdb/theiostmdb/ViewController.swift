8983093fc5aec79c0516ede862b782dccd653599
david gutierrez <davidjose.gutierrezcalderon@cgi.com>
2017-03-10 12:29:51 +0100

    initial commit

---
+++ theiostmdb/theiostmdb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  theiostmdb
+//
+//  Created by davidjose gutierrez calderon  on 10/3/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
