26fcde58211b76bd6af5fa10fdf97f2f85eef2b2
david gutierrez <davidjose.gutierrezcalderon@cgi.com>
2017-03-10 13:31:29 +0100

    Added Network Service

--- theiostmdb/theiostmdb/Film.swift
+++ theiostmdb/theiostmdb/Film.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+struct Film {
+    let poster_path : String
+    let overview : String
+    let release_date : String
+    let title : String
+    
+    // MARK: - Initializers
+    init(dictionary: [String: AnyObject]) {
+        self.poster_path = dictionary["poster_path"] as! String
+        self.overview = dictionary["overview"] as! String
+        self.release_date = dictionary["release_date"] as! String
+        self.title = dictionary["title"] as! String
+    }
+}
--- theiostmdb/theiostmdb/NetworkService.swift
+++ theiostmdb/theiostmdb/NetworkService.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import Alamofire
+
+class NetworkService {
+    
+    let api_key = "93aea0c77bc168d8bbce3918cefefa45"
+    let base_url = "https://api.themoviedb.org/3/"
+    let images_url = "https://image.tmdb.org/t/p/w500/"
+    let discover_url = "discover/movie?api_key=%1$;language=en-US;sort_by=popularity.desc;include_adult=false;include_video=false;page="
+    let search_url = "https://api.themoviedb.org/3/search/movie?api_key=%1$s&amp;language=en-US&amp;query=%2$s&amp;page=%3$s&amp;include_adult=false"
+    
+    let shared = NetworkService()
+    
+    
+    
+}
