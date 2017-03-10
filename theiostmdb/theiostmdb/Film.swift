263637e8ebae883ed7029e3ca15d8ea6adcd31e8
david gutierrez <davidjose.gutierrezcalderon@cgi.com>
2017-03-10 13:30:28 +0100

    added Alamofire

---
+++ theiostmdb/theiostmdb/Film.swift
@@ -0,0 +1,9 @@
+//
+//  Film.swift
+//  theiostmdb
+//
+//  Created by davidjose gutierrez calderon  on 10/3/17.
+//
+//
+
+import Foundation
---
+++ theiostmdb/theiostmdb/NetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkService.swift
+//  theiostmdb
+//
+//  Created by davidjose gutierrez calderon  on 10/3/17.
+//
+//
+
+import Foundation
